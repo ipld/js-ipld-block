135e3b66cee19272085e857587a595fca20bdc75
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2017-03-15 22:07:11 +0100

    feat: simplify block to only be data and cid

---
+++ test/index.spec.js
@@ -0,0 +1,45 @@
+/* eslint-env mocha */
+'use strict'
+
+const expect = require('chai').expect
+const CID = require('cids')
+
+const Block = require('../src')
+
+describe('block', () => {
+  it('create throws', () => {
+    expect(
+      () => new Block('string')
+    ).to.throw()
+
+    expect(
+      () => new Block(new Buffer('hello'), 'cid')
+    ).to.throw()
+
+    expect(
+      () => new Block('hello', new CID('QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n'))
+    ).to.throw()
+  })
+
+  it('create', () => {
+    const b = new Block(new Buffer('hello'), new CID('QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n'))
+
+    expect(Block.isBlock(b)).to.eql(true)
+  })
+
+  it('block stays immutable', () => {
+    const b = new Block(new Buffer('hello'), new CID('QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n'))
+
+    expect(
+      () => { b.data = 'fail' }
+    ).to.throw(
+      /immutable/
+    )
+
+    expect(
+      () => { b.cid = 'fail' }
+    ).to.throw(
+      /immutable/
+    )
+  })
+})
