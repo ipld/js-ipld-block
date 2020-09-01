7c0a6df66cc0c5ab527df66631c9d4ab94b74c4b
Xmader <xmader@outlook.com>
2020-09-01 07:44:17 -0400

    TypeScript support (#55)
    Some tricks are needed for the TypeScript support.

--- src/index.js
+++ src/index.js
@@ -6,14 +6,15 @@ const withIs = require('class-is')
 /**
  * Represents an immutable block of data that is uniquely referenced with a cid.
  *
- * @constructor
- * @param {Uint8Array} data - The data to be stored in the block as a Uint8Array.
- * @param {CID} cid - The cid of the data
- *
  * @example
  * const block = new Block(Uint8Array.from([0, 1, 2, 3]), new CID('...'))
  */
-class Block {
+module.exports = class Block {
+  /**
+   * @constructor
+   * @param {Uint8Array} data - The data to be stored in the block as a Uint8Array.
+   * @param {CID} cid - The cid of the data
+   */
   constructor (data, cid) {
     if (!data || !(data instanceof Uint8Array)) {
       throw new Error('first argument  must be a Uint8Array')
@@ -52,6 +53,17 @@ class Block {
   set cid (val) {
     throw new Error('Tried to change an immutable block')
   }
+
+  // eslint-disable-next-line valid-jsdoc
+  /**
+   * Check if the given value is a Block.
+   * @returns {other is Block}
+   */
+  static isBlock (other) { // eslint-disable-line no-unused-vars
+    // implemented by class-is module
+  }
 }
 
-module.exports = withIs(Block, { className: 'Block', symbolName: '@ipld/js-ipld-block/block' })
+// to trick the typings engine
+// https://github.com/ipld/js-ipld-block/pull/55#discussion_r478845002
+module.exports = withIs(module.exports, { className: 'Block', symbolName: '@ipld/js-ipld-block/block' })
